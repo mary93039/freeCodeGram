3832ae18aa9f08b32d24c9fe0c8814274df52c78
mary79930 <mary93039@hotmail.com>
2019-12-07 16:16:02 +0800

    adding whole freeCodeGram

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Cache;
+use App\User;
+
+class ProfilesController extends Controller
+{
+    public function index(User $user)
+    {
+        $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+        
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user){
+                return $user->posts->count();
+            });
+
+        $followersCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user){
+                return $user->profile->followers->count();
+            });
+
+        $followingCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user){
+                return $user->following->count();
+            });
+        // $postCount = $user->posts->count();
+        // $followersCount = $user->profile->followers->count();
+        // $followingCount = $user->following->count();
+        return view('profiles.index', compact('user','follows','postCount','followersCount','followingCount'));
+        // $user = User::findOrFail($user);
+        // return view('profiles.index', [
+        //     'user' => $user,
+        // ]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+        
+
+        if(request('image'))
+        {
+            $imagePath = request('image')->store('profile','public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+
+            $imageArray = ['image' => $imagePath];
+        }
+
+        auth()->user()->profile->update($data);
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            $imageArray ?? []
+        ));
+        return redirect("/profile/{$user->id}");
+
+        // return view('profiles.update', compact('user'));
+    }
+    
+}
